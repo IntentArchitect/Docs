3714a0c3ae097467ba6586f3b65fac6c0da97a53
dandrejvv <dandrejvv@gmail.com>
2021-12-09 00:53:40 +0200

    Our first extension plugin for docfx to introduce markdown style video specifications.
    Apparently there is a notation for this according to VSCode's Markdown linter.

---
+++ docfx-extensions/Intent.DocFx.TagExtensions/MarkdownVideoTag/MarkdownVideoInlineRule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.DocAsCode.MarkdownLite;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Intent.DocFx.TagExtensions.MarkdownVideoTag
+{
+    public class MarkdownVideoInlineRule : IMarkdownRule
+    {
+        public virtual string Name => "MarkdownVideo";
+
+        private static readonly Regex VideoRegex = new Regex(@"^:::video source=""([a-zA-Z0-9\:\.\\\/_\-&;]+)"":::", RegexOptions.Compiled);
+
+        public virtual IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
+        {
+            var match = VideoRegex.Match(context.CurrentMarkdown);
+            if (match.Length == 0)
+            {
+                return null;
+            }
+            var sourceInfo = context.Consume(match.Length);
+            var videoSrc = match.Groups[1].Value;
+            return new MarkdownVideoInlineToken(this, parser.Context, videoSrc, sourceInfo);
+        }
+    }
+}
