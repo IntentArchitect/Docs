a2cea099f3119415b701bb7fad69d576138a1182
dandrejvv <dandrejvv@gmail.com>
2022-02-03 15:39:18 +0200

    Updated the spacing to reduce linter warnings

--- Template/darkerfx_custom/styles/plugin-featherlight.js
+++ Template/darkerfx_custom/styles/plugin-featherlight.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //find all images, but not the logo, and add the lightbox
-    $('img').not('#logo').each(function(){
+    $('img').not('#logo').each(function () {
         var $img = $(this);
-        var filename = $img.attr('src')
+        var filename = $img.attr('src');
         //add cursor
-        $img.css('cursor','zoom-in');
-        $img.css('cursor','-moz-zoom-in');
-        $img.css('cursor','-webkit-zoom-in');
+        $img.css('cursor', 'zoom-in');
+        $img.css('cursor', '-moz-zoom-in');
+        $img.css('cursor', '-webkit-zoom-in');
 
         //add featherlight
         $img.attr('alt', filename);
         $img.featherlight(filename);
-});
+    });
 });
