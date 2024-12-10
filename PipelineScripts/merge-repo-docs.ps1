c034a53f4b7acff8dda64775e5cf1c4543d613b5
JP Seini <jp.seini@intentarchitect.com>
2024-12-10 10:17:04 +0200

    Merge script into pipeline

---
+++ PipelineScripts/merge-repo-docs.ps1
@@ -0,0 +1,119 @@
+# This script should be executed from the root folder of the "Docs" repo
+# Example execution: .\PipelineScripts\merge-repo-docs.ps1 "development-4.4x" "https://github.com/IntentArchitect/Intent.Modules.NET.git" "Intent.Modules.NET" "modules-dotnet" 0
+
+param(
+    # The source branch name
+    [string]$sourceBranchName,
+    # The module repo URL
+    [string]$moduleRepositoryUrl,
+    # The default folder in which the module will be cloned into
+    [string]$moduleFolderName,
+    # The The name of the folder in docs where this files should go
+    [string]$moduleOutputFolder,
+    # https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#system-variables-devops-services
+    [bool]$isOnBuildAgent = $($env:TF_BUILD -eq "True")
+)
+
+Write-Host "`$sourceBranchName=$sourceBranchName"
+Write-Host "`$moduleRepositoryUrl=$moduleRepositoryUrl"
+Write-Host "`$moduleFolderName=$moduleFolderName"
+Write-Host "`$moduleOutputFolder=$moduleOutputFolder"
+Write-Host "`$isOnBuildAgent=$isOnBuildAgent"
+
+# clean the folder incase it wasn't before
+git clean -fdx
+
+# clone only if the destination folder doesn't already exist
+# destination folder should never exist as the git clean above should have removed it
+# but if it does exists for whatever reason, a git pull is done later to get the latest
+if (-not (Test-Path -Path $moduleFolderName)) {
+    Write-Host "Cloning module repo"
+    git clone $moduleRepositoryUrl
+}
+
+cd $moduleFolderName
+
+# Check the remote to see if a branch exists which matches the source branch
+$branchExists = git ls-remote --heads origin $sourceBranchName | Select-String -Pattern $sourceBranchName
+
+# either use the source branch name if it exists, or default to "development"
+if ($branchExists) {
+    Write-Host "Checkout $sourceBranchName"
+    git checkout $sourceBranchName
+    git pull
+} else {
+    Write-Host "Checkout development"
+    git checkout development
+    git pull
+}
+
+# detach the folder from git so can be cleaned up
+if (Test-Path -Path .git) {
+     Remove-Item -Recurse -Force .git
+}
+
+# get the full module path for later use. This is where the repo was cloned to
+$fullModulePath = (Get-Location).Path
+cd ..
+
+# get all "README.md" files in a docs folder under the module folder name
+Write-Host "Get-ChildItem -Path ""$fullModulePath"" -Recurse -Filter ""README.md"" | Where-Object { $_.DirectoryName -match '\\docs$' }"
+$files = Get-ChildItem -Path "$fullModulePath" -Recurse -Filter "README.md" | Where-Object { $_.DirectoryName -match '\\docs$' }
+
+$numberOfFiles = $files.Count
+Write-Host "Number of files found: $numberOfFiles"
+
+# switch to this folder as makes easier to build up destination folder paths later
+cd "articles"
+
+foreach ($file in $files) {
+    Write-Host "Found matching file: $($file.FullName)"
+
+    # Scan the file contents for the markdown header (starts with #)
+    $firstHeader = Get-Content $($file.FullName) | Where-Object { $_ -match '^#' } | Select-Object -First 1
+    if ($firstHeader) {
+
+        # strip out the # from the header, to be left with the module name
+        $moduleName = $firstHeader -replace '^#\s*', ''  
+        
+        # here we are in the "articles" folder of "Docs". Append the module destination folder
+        $destinationRepoFolder = Join-Path -Path (Get-Location) -ChildPath $moduleFolderName
+        
+        # To lower the module name and replace the "." with "-"
+        $destinationModuleFolderName = $moduleName.ToLower() -replace '\.', '-'
+        $destinationModuleFileName = "$destinationModuleFolderName.md"
+
+        # the full output path
+        $fullDestinationFolder = Join-Path -Path $moduleOutputFolder -ChildPath $destinationModuleFolderName
+
+        # Check if the folder structure exists; if not, create it
+        if (-not (Test-Path -Path $fullDestinationFolder)) {
+            New-Item -Path $fullDestinationFolder -ItemType Directory -Force
+            Write-Host "Folder structure created: $fullDestinationFolder"
+        } else {
+            Write-Host "Folder structure already exists: $fullDestinationFolder"
+        }
+
+        # final output with the folder + filename
+        $destinationFullPath = Join-Path -Path $fullDestinationFolder -ChildPath $destinationModuleFileName
+
+        Write-Host "Copying '$($file.FullName)' to '$destinationFullPath'"
+        Copy-Item -Path $($file.FullName) -Destination $destinationFullPath
+
+        # copy images folder if it exists
+        $sourceFolder = [System.IO.Path]::GetDirectoryName($file.FullName)
+        $sourceFolder = Join-Path -Path $sourceFolder -ChildPath "images"
+        $imagesFolder = Join-Path -Path $fullDestinationFolder -ChildPath "images"
+
+        if (Test-Path -Path $sourceFolder) {
+            Write-Host "Copying images folder: '$sourceFolder'"
+            Copy-Item -Path $sourceFolder -Destination $imagesFolder -Force -Recurse
+        } else {
+            Write-Host "No images folder to copy: '$sourceFolder'"
+        }
+    } else {
+        Write-Host "No header found - skipping file"
+    }
+}    
+
+cd..
